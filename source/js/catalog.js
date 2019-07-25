679602758ceff4566a2671cba0e70c2ea0ff8405
Артём <tyomik1000@gmail.com>
2019-07-25 13:36:43 +0300

    Сделал разметку страницы каталога

--- source/js/catalog.js
+++ source/js/catalog.js
@@ -1,12 +1,12 @@
 const headerNav = document.querySelector(".main-nav");
 const header = document.querySelector(".header");
 const headerButton = document.querySelector(".main-nav__toggle");
-const link = document.querySelector(".advantages__link");
-const advantage = document.querySelector(".advantages-popup");
-const close = advantage.querySelector(".advantages-popup__close");
 const headerLogoMobile = document.querySelector(".header__logo-image");
 const headerLogoTablet = document.querySelector(".header__logo-image-tablet");
 const headerLogoDesktop = document.querySelector(".header__logo-image-desktop");
+const filterButton = document.querySelector(".country-filter__toggle");
+const close = document.querySelector(".country-filter-popup__close");
+const filter = document.querySelector(".country-filter");
 
 
 headerButton.addEventListener("click", function (evt) {
@@ -22,25 +22,6 @@ headerButton.addEventListener("click", function (evt) {
   }
 });
 
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  advantage.classList.remove("visually-hidden");
-});
-
-close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  advantage.classList.add("visually-hidden");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (advantage.classList.contains("visually-hidden") === false) {
-      advantage.classList.add("visually-hidden");
-    }
-  }
-});
-
 window.onscroll = function showHeader() {
   if(window.pageYOffset > 200){
     header.classList.add('header--scroll');
@@ -53,6 +34,23 @@ window.onscroll = function showHeader() {
     headerLogoTablet.srcset='img/logo-tablet-white@1x.png';
     headerLogoDesktop.srcset='img/logo-desktop-white@1x.png';
   }
-}
+};
 
-// 'img/logo-mobile-blue@1x.png'
+filterButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  filter.classList.toggle("country-filter--closed");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  filter.classList.add("country-filter--closed");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (filter.classList.contains("country-filter--closed") === false) {
+      filter.classList.add("country-filter--closed");
+    }
+  }
+});
