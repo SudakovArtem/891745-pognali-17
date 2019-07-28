1ae56385ba72877619f7bf3ef7d74bdf0de06c76
Артём <tyomik1000@gmail.com>
2019-07-28 11:05:39 +0300

    Закончил стилизацию страницы формы

---
+++ source/js/form.js
@@ -0,0 +1,56 @@
+const headerNav = document.querySelector(".main-nav");
+const header = document.querySelector(".header");
+const headerButton = document.querySelector(".main-nav__toggle");
+const headerLogoMobile = document.querySelector(".header__logo-image");
+const headerLogoTablet = document.querySelector(".header__logo-image-tablet");
+const headerLogoDesktop = document.querySelector(".header__logo-image-desktop");
+const countryButton = document.querySelector(".plan-filter__button--choice");
+const close = document.querySelector(".plan-add-popup__close");
+const countryPopup = document.querySelector(".plan-add-popup");
+
+
+headerButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  headerNav.classList.toggle("main-nav--open");
+  header.classList.toggle("header--open");
+  if (header.classList.contains("header--open") === true) {
+    headerLogoMobile.src = 'img/logo-mobile-blue@1x.png';
+    headerLogoTablet.srcset = 'img/logo-tablet-blue@1x.png';
+  } else {
+    headerLogoMobile.src = 'img/logo-mobile-white@1x.png';
+    headerLogoTablet.srcset = 'img/logo-tablet-white@1x.png';
+  }
+});
+
+window.onscroll = function showHeader() {
+  if(window.pageYOffset > 200){
+    header.classList.add('header--scroll');
+    headerLogoMobile.src='img/logo-mobile-blue@1x.png';
+    headerLogoTablet.srcset='img/logo-tablet-blue@1x.png';
+    headerLogoDesktop.srcset='img/logo-desktop-blue@1x.png';
+  } else{
+    header.classList.remove('header--scroll');
+    headerLogoMobile.src='img/logo-mobile-white@1x.png';
+    headerLogoTablet.srcset='img/logo-tablet-white@1x.png';
+    headerLogoDesktop.srcset='img/logo-desktop-white@1x.png';
+  }
+};
+
+countryButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  countryPopup.classList.toggle("plan-add-popup--open");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  countryPopup.classList.remove("plan-add-popup--open");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (countryPopup.classList.contains("plan-add-popup--open") === true) {
+      countryPopup.classList.remove("plan-add-popup--open");
+    }
+  }
+});
